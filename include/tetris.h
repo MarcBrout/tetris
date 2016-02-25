56382ce0a01de8a29723cfca8b56275c240a1704
Marc <brout.marc@gmail.com>
2016-02-25 17:12:31 +0100

    ready

---
+++ include/tetris.h
@@ -0,0 +1,19 @@
+/*
+** tetris.h for tetris in /home/brout_m/RENDU/PSU/PSU_2015_tetris
+**
+** Made by marc brout
+** Login   <brout_m@epitech.net>
+**
+** Started on  Thu Feb 25 16:49:29 2016 marc brout
+** Last update Thu Feb 25 16:55:52 2016 marc brout
+*/
+
+#ifndef TETRIS_H_
+# define TETRIS_H_
+
+typedef struct		s_tetris
+{
+
+}			t_tetris;
+
+#endif /* !TETRIS_H_ */
